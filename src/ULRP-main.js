365ff619c45b44930a9f6a589d707589af14b055
Oopsno <yufeiliu94@gmail.com>
2016-04-11 22:39:19 +0800

    ULRP中开始加入iframe

--- src/ULRP-main.js
+++ src/ULRP-main.js
@@ -1,6 +1,4 @@
 (function () {
   'use strict';
-  unsafeWindow.frames.topFrame.onload = function () {
-    ULRP.Hack.hackTopFrame(unsafeWindow.frames.topFrame.window);
-  };
+  ULRP.W.top.onload = () => ULRP.Hack.hackTopFrame(ULRP.W.top);
 })();
--- src/core.js
+++ src/core.js
@@ -2,4 +2,19 @@
  * ULRP - Core Setup
  */
 
-let ULRP = {};
+let ULRP = ((ULRP) => {
+  "use strict";
+  ULRP.W = {
+    top: unsafeWindow.frames.topFrame.window
+  };
+  ULRP.D = {
+    top: unsafeWindow.frames.topFrame.window.document
+  };
+  ULRP.$ = {
+    top: $(unsafeWindow.frames.topFrame.window.document)
+  };
+  ULRP.F = {
+    top: unsafeWindow.frames.topFrame
+  };
+  return ULRP;
+})({});
